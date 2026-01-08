63efb7f476f22b88efc8fdc6fafaf647b95dcd60
Joenio Silva <137446909+JoenioSampaio@users.noreply.github.com>
2026-01-08 07:29:18 -0300

    Atualização de projetos python

---
+++ identacao_blocos.py
@@ -0,0 +1,10 @@
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= 500:
+        print("valor sacado!")
+        print("retire o sei dinheiro na boca do caixa.")
+
+       
+    print("Obrigado por ser nossso cliente, tenha um bom dia!")
+
---
+++ operadores_de_associacao.py
@@ -0,0 +1,6 @@
+frutas = ["limão", "uva", "laranja"]
+curso = "curso de python"
+
+print("laranja" in frutas)
+print("limão" in frutas)
+print("python" in curso)
