9a5d30fb333d9174d6e697f21e309e4165b2bb0c
Joenio Silva <joenio.sampaio@gmail.com>
2026-02-18 08:24:57 -0300

    atualização

---
+++ Manipulando_String_Com_Python/Tipos_String.py
@@ -0,0 +1,24 @@
+nome = "Joenio"
+
+mensagem = f"""
+    Olá, meu nome é {nome},
+Eu estou aprendendo Python.
+    Essa mensagem tem diferentes recursos.
+"""
+################Opcao###################
+
+print(mensagem)
+
+print("""
+      ===========MENU============
+      
+      1 - Deposito
+      2 - Sacar
+      0 - Sair
+
+      ==========================================
+
+      Obrigado por usar nosso sistema!!!
+
+      """
+      )
